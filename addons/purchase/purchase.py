6c6049967e57150bfcb58f1a03cb9ff203df7b62
florian-dacosta <florian.dacosta@akretion.com>
2015-04-08 13:22:52 +0200

    [REF] add hook on method make_po to ease overriding the merge of automatic po

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -1333,6 +1333,23 @@ class procurement_order(osv.osv):
             'taxes_id': [(6, 0, taxes)],
         }
 
+    def get_available_draft_po_ids(self, cr, uid, procurement, partner,
+                                   context=None):
+        """ 
+            Return a list of existing draft po ids we can use to avoid 
+            creating a new one.
+        """
+        po_obj = self.pool['purchase.order']
+        available_draft_po_ids = po_obj.search(cr, uid,
+            [('partner_id', '=', partner.id),
+             ('state', '=', 'draft'),
+             ('picking_type_id', '=', procurement.rule_id.picking_type_id.id),
+             ('location_id', '=', procurement.location_id.id),
+             ('company_id', '=', procurement.company_id.id),
+             ('dest_address_id', '=', procurement.partner_dest_id.id)],
+            context=context)
+        return available_draft_po_ids
+
     def make_po(self, cr, uid, ids, context=None):
         """ Resolve the purchase from procurement, which may result in a new PO creation, a new PO line creation or a quantity change on existing PO line.
         Note that some operations (as the PO creation) are made as SUPERUSER because the current user may not have rights to do it (mto product launched by a sale for example)
@@ -1357,9 +1374,9 @@ class procurement_order(osv.osv):
                 purchase_date = self._get_purchase_order_date(cr, uid, procurement, company, schedule_date, context=context) 
                 line_vals = self._get_po_line_values_from_proc(cr, uid, procurement, partner, company, schedule_date, context=context)
                 #look for any other draft PO for the same supplier, to attach the new line on instead of creating a new draft one
-                available_draft_po_ids = po_obj.search(cr, uid, [
-                    ('partner_id', '=', partner.id), ('state', '=', 'draft'), ('picking_type_id', '=', procurement.rule_id.picking_type_id.id),
-                    ('location_id', '=', procurement.location_id.id), ('company_id', '=', procurement.company_id.id), ('dest_address_id', '=', procurement.partner_dest_id.id)], context=context)
+                available_draft_po_ids = self.get_available_draft_po_ids(
+                                                cr, uid, procurement, partner,
+                                                context=context)
                 if available_draft_po_ids:
                     po_id = available_draft_po_ids[0]
                     po_rec = po_obj.browse(cr, uid, po_id, context=context)
